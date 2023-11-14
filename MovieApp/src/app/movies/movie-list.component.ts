1b53c53e977d494182fdf9e408e9f0f525af064f
Andreas Daxer <info@andreas-daxer.de>
2023-11-14 10:16:25 +0100

    MovieService in MovieListComponent plus NgFor

--- MovieApp/src/app/movies/movie-list.component.ts
+++ MovieApp/src/app/movies/movie-list.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { MovieService } from '../services/movie.service';
+import { Movie } from '../models/movie';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgFor],
   templateUrl: './movie-list.component.html',
   styles: ``
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit {
+  movies:  Movie[]=[];
+  constructor(private movieService: MovieService){
+  }
 
+  ngOnInit(): void {
+    this.movies = this.movieService.getMovies();
+  }
 }
